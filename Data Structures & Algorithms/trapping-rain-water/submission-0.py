c00d535110ed0aea6664e6fb0c8ea1c7d5130c0d
Prateek Narendra <prateeknar@gmail.com>
2026-08-13 21:49:54 -0700

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        ans, stack = 0, list()
+        cur, leng = 0, len(height)
+        while cur < leng:
+            while len(stack) != 0 and height[cur] > height[stack[-1]]:
+                top = stack[-1]
+                stack.pop()
+                if len(stack) == 0:
+                    break
+                distance = cur - stack[-1] - 1
+                bounded_height = min(height[cur], height[stack[-1]]) - height[top]
+                ans += distance * bounded_height
+            stack.append(cur)
+            cur += 1
+        return ans
